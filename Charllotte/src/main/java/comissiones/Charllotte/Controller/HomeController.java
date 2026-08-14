0b02adddcc5a040a65f7132a7c8a2164ec00ff6c
Daniel Costa Talevi <danielctalevi@gmail.com>
2026-08-14 19:04:28 -0300

    Update and rename TesteController.java to HomeController.java

---
+++ Charllotte/src/main/java/comissiones/Charllotte/Controller/HomeController.java
@@ -0,0 +1,16 @@
+package comissiones.Charllotte.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String mostrarHome() {
+        return "home";
+    }
+
+}
